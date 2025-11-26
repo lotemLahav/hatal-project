caa9f573251dabda958455f74f6afc02713cbe1b
Lotem Lahav <lotems.projects@gmail.com>
2025-11-26 17:28:04 +0200

    refactor(user.entity.ts): create the user table

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, nullable: false })
+  username: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  phone: number;
+}
